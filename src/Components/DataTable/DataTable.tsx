3e60d8753037dbe6533a5196afaf481ddaddbeed
Sameer <sameersattar1111@gmail.com>
2023-11-06 10:31:36 +0500

    dashboard design  commit

--- src/Components/DataTable/DataTable.tsx
+++ src/Components/DataTable/DataTable.tsx
@@ -1,15 +1,36 @@
 import "./DataTable.scss"
 import { DataGrid, GridColDef, GridToolbar , GridValueGetterParams } from '@mui/x-data-grid';
 import Box from '@mui/material/Box';
+import { Link } from "react-router-dom";
 
 type Props = {
   columns : GridColDef[],
-  rows : object[]
+  rows : object[],
+  slug: string
 }
 
 
   
 const DataTable = (props:Props) => {
+
+    const actionColumn:GridColDef = {
+      field:"action",
+      headerName : "Action",
+      width:20,
+      renderCell:(params)=> {
+        return(
+          <div className="action">
+            <Link to="">
+            <img src="/view.svg" alt="" />
+            </Link>
+            <Link className="delete" to="">
+            <img src="/delete.svg" alt="" />
+            </Link>
+          </div>
+        )
+      }
+    }
+
   return (
     <Box sx={{ height: 400, width: '100%' }}>
       <DataGrid
@@ -19,7 +40,7 @@ const DataTable = (props:Props) => {
         initialState={{
           pagination: {
             paginationModel: {
-              pageSize: 15,
+              pageSize: 10,
             },
           },
         }}
--- src/Pages/Users/Users.tsx
+++ src/Pages/Users/Users.tsx
@@ -62,7 +62,7 @@ const Users = () => {
         <h1>Users</h1>
         <button>Add New Users</button>
       </div>
-      <DataTable columns={columns} rows={userRows} />
+      <DataTable slug="users" columns={columns} rows={userRows} />
     </div>
     </Layout>
   )
